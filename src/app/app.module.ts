f53fb87a6cf9c47a66a30abe34b148fa4a6eaed6
Coen van Campenhout <coenvc@gmail.com>
2017-09-19 11:45:53 +0200

    Added routing and tinyeditor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TinyEditorComponent } from 'components/tiny-editor/tiny-editor.component';
+import { KioskComponent } from 'components/kiosk/kiosk.component';
+
+const appRoutes : Routes =[
+  {path: 'kiosk', component: KioskComponent}
+] 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TinyEditorComponent,
+    KioskComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+}) 
+
 export class AppModule { }
