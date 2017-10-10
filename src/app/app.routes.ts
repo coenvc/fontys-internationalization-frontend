ec86a297ea01529f7017c1c486d7f6ffe03aa660
Coen van Campenhout <coenvc@gmail.com>
2017-10-10 11:37:47 +0200

    Added blog component with tinymce editor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
 import {KioskComponent} from 'components/kiosk/kiosk.component';
 import { TinyEditorComponent } from 'components/tiny-editor/tiny-editor.component';
+import { BlogComponent } from 'components/blog/blog.component';
 
 const APP_ROUTES: Routes = [
   {path: 'kiosk', component: KioskComponent}, 
-  {path: 'tiny-editor', component: TinyEditorComponent}
+  {path: 'tiny-editor', component: TinyEditorComponent},
+  {path: 'blog', component: BlogComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES, {useHash: true});
