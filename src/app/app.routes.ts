6f06b8f81e131454ff446d8754044a98123a715e
reinoudvanzoelen <reinoudvanzoelen>
2017-10-31 13:51:32 +0100

    Added DetailComponent to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {KioskComponent} from 'components/kiosk/kiosk.component';
 import {TinyEditorComponent} from 'components/tiny-editor/tiny-editor.component';
 import {BlogComponent} from 'components/blog/blog.component';
 import {LoginComponent} from "../components/login/login.component";
+import {DetailComponent} from "../components/detail-page/detail.component";
 
 const APP_ROUTES: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'kiosk', component: KioskComponent},
   {path: 'tiny-editor', component: TinyEditorComponent},
   {path: 'blog', component: BlogComponent},
-  {path: '', component: LoginComponent}
+  {path: 'detail-page', component: DetailComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES, {useHash: true});
