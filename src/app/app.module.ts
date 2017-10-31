4fffde02e5d88a5c1fc67d17e763e8cb04bfe682
reinoudvanzoelen <reinoudvanzoelen>
2017-10-31 13:42:49 +0100

    Cleaned up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { TinyEditorComponent } from 'components/tiny-editor/tiny-editor.component';
-import { KioskComponent } from 'components/kiosk/kiosk.component';
-import { HeaderComponent } from 'components/header/header.component';
-import {routing} from 'app/app.routes';
-import { MapComponent } from 'components/map/map.component';
-import { BlogComponent } from 'components/blog/blog.component';
-import{HttpClientService} from "./database/HttpClientService";
-import {BlogDataservice} from "./database/blog.dataservice";
+// Angular components
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 import {HttpModule} from "@angular/http";
 
+// Globally used components
+import {AppComponent} from './app.component';
+import {routing} from 'app/app.routes';
+import {HttpClientService} from "./database/HttpClientService";
+
+// Developed components
+import {TinyEditorComponent} from 'components/tiny-editor/tiny-editor.component';
+import {KioskComponent} from 'components/kiosk/kiosk.component';
+import {HeaderComponent} from 'components/header/header.component';
+import {MapComponent} from 'components/map/map.component';
+import {BlogComponent} from 'components/blog/blog.component';
+import {LoginComponent} from "../components/login/login.component";
+
+// DataServices
+import {LoginDataservice} from "./database/login.dataservice";
+import {BlogDataservice} from "./database/blog.dataservice";
+import {UserDataservice} from "./database/user.dataservice";
 
 @NgModule({
   declarations: [
@@ -21,7 +30,8 @@ import {HttpModule} from "@angular/http";
     KioskComponent,
     HeaderComponent,
     MapComponent,
-    BlogComponent
+    BlogComponent,
+    LoginComponent
   ],
   imports: [
     routing,
@@ -30,8 +40,11 @@ import {HttpModule} from "@angular/http";
     HttpModule
   ],
   providers: [HttpClientService,
-    BlogDataservice],
+    LoginDataservice,
+    BlogDataservice,
+    UserDataservice],
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
