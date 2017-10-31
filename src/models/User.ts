902e367ea28e584e2b552c2485796e3cb6ba9406
reinoudvanzoelen <reinoudvanzoelen>
2017-10-31 13:39:03 +0100

    Added ID to user constructor

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,15 @@
-export class User{
+export class User {
 
-    id: number;
-     firstName: String;
-     lastName: String;
-     infix: String;
-     email: String;
-     password: String;
-     isModerator: Boolean;
+  public id: number;
+  public firstName: String;
+  public lastName: String;
+  public infix: String;
+  public email: String;
+  public password: String;
+  public isModerator: Boolean;
 
-  constructor(firstName?: String, lastName?: String, infix?: String, email?: String, password?: String, isModerator?: Boolean){
-
+  constructor(id?: number, firstName?: String, lastName?: String, infix?: String, email?: String, password?: String, isModerator?: Boolean) {
+    this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.infix = infix;
