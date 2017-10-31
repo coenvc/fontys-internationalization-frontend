b24c79d7b75221fb90c567161463a82d4bc7b618
reinoudvanzoelen <reinoudvanzoelen>
2017-10-31 13:39:44 +0100

    Added the login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import {RouterModule, Routes} from '@angular/router';
 import {KioskComponent} from 'components/kiosk/kiosk.component';
-import { TinyEditorComponent } from 'components/tiny-editor/tiny-editor.component';
-import { BlogComponent } from 'components/blog/blog.component';
+import {TinyEditorComponent} from 'components/tiny-editor/tiny-editor.component';
+import {BlogComponent} from 'components/blog/blog.component';
+import {LoginComponent} from "../components/login/login.component";
 
 const APP_ROUTES: Routes = [
-  {path: 'kiosk', component: KioskComponent}, 
+  {path: 'kiosk', component: KioskComponent},
   {path: 'tiny-editor', component: TinyEditorComponent},
-  {path: 'blog', component: BlogComponent}
+  {path: 'blog', component: BlogComponent},
+  {path: '', component: LoginComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES, {useHash: true});
---
+++ src/components/login/login.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {LoginDataservice} from "../../app/database/login.dataservice";
+import {User} from "../../models/User";
+import {Router} from '@angular/router';
+
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  constructor(private loginService: LoginDataservice, private router: Router) {
+  }
+
+  private user: User;
+
+  ngOnInit() {
+    localStorage.setItem('currentUser', null);
+  }
+
+  onSubmit(input) {
+    this.loginService.login(input.email, input.password)
+      .subscribe(
+        result => this.login(result)
+      );
+  }
+
+  private login(res) {
+    this.user = res;
+
+    console.log(this.user.email);
+
+    if (this.user.email == null){
+      alert('Gebruiker niet gevonden.');
+      return null;
+    } else {
+      this.user = res;
+      localStorage.setItem('currentUser', JSON.stringify(this.user));
+
+      alert("Succesvol ingelogd");
+      console.log('User successfully logged in');
+      //TODO: Add a navigation after successfully loggin in
+      //this.router.navigate(['dashboard']);
+    }
+  }
+}
