6929e3adcdae9220320023635155d5b015ea38c4
ReinoudvanZoelen <f4n9yEBbdrIe5CTaRxdV>
2017-11-28 15:02:12 +0100

    First draft (gets data but isn't pretty) of Dashboard and edited User to match new data in backend

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,16 +6,21 @@ export class User {
   public infix: String;
   public email: String;
   public password: String;
+  public study: String;
+  public active: boolean;
   public isModerator: Boolean;
 
+
   constructor(id?: number, firstName?: String, lastName?: String, infix?: String, email?: String, password?: String,
-              isModerator?: Boolean) {
+              study?: String, active?: boolean, isModerator?: Boolean) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.infix = infix;
     this.email = email;
     this.password = password;
+    this.study = study;
+    this.active = active;
     this.isModerator = isModerator;
   }
 }
